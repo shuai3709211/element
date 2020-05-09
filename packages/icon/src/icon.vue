f13edb6e7d29036613696c70923e5a4beed3c562
shuai <tianji_fengliu@126.com>
2020-05-09 16:35:15 +0800

    1

--- packages/icon/src/icon.vue
+++ packages/icon/src/icon.vue
@@ -1,7 +1,5 @@
 <template>
-  <svg v-if="isElIcon"
-          class="icon"
-          :style="{ fontSize: size + 'px', color: color }"
+  <svg v-if="!isElIcon"
           aria-hidden="true"
   >
     <use :xlink:href="'#' + name"></use>
@@ -32,12 +30,4 @@
     }
   };
 </script>
-<style scoped>
-  .icon {
-    width: 1em;
-    height: 1em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
-  }
-</style>
+
